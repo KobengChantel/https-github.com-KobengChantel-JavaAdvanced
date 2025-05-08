ddbf00521fb4538bc35fefb4c0a7c313731a6b03
Kobeng Chantel <kobengchantel@gmail.com>
2025-05-08 10:03:56 +0200

    done

---
+++ ig2/src/Lesson15/AtomicCounterRunnable.java
@@ -0,0 +1,28 @@
+package Lesson15;
+
+public class AtomicCounterRunnable implements Runnable {
+    private AtomicCounter count = new AtomicCounter();
+    private int countSize = 0;
+    private String threadName;
+
+    public AtomicCounterRunnable(){
+        super();
+    }
+
+    public AtomicCounterRunnbble(int countSize, String threadName){
+        this.countSize = countSize;
+        this.threadName = threadName;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < countSize; i++) {
+            count.increment();
+            System.out.println(threadName + "Current Count:" + this.getCounter());
+        }
+    }
+        public synchronized int getCounter(){
+            return count.getValue();
+        }
+
+}
