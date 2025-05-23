58d9513a8fb10f52a4dd47dfebcffcc7b1b121d4
Kobeng Chantel <kobengchantel@gmail.com>
2025-05-23 16:03:55 +0200

    done

--- ig2/src/Lesson12/DateTimeFormatterExample.java
+++ ig2/src/Lesson12/DateTimeFormatterExample.java
@@ -1,7 +1,22 @@
-
-
+/**
+ * To change this license header,
+ * choose License Headers in Project Properties
+ * To change this template file, choose Tools | Templates
+ *and open the template in the editor
+ */
 
 package Lesson12;
 
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+/**
+ * @author tmcginn
+ */
 public class DateTimeFormatterExample {
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();
+
+    }
 }
