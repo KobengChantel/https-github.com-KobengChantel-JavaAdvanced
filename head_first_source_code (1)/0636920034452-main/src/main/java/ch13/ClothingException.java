3be26f8673668be787c886cd1bc94da329e9880a
Kobeng Chantel <kobengchantel@gmail.com>
2025-12-28 23:00:29 +0200

    done with java

--- head_first_source_code (1)/0636920034452-main/src/main/java/ch13/ClothingException.java
+++
@@ -1,7 +0,0 @@
-package ch13;
-
-// A custom checked exception representing errors related to clothing operations.
-
-public class ClothingException extends Exception {
-    // No additional fields or methods; inherits standard Exception behavior.
-}
--- head_first_source_code (1)/0636920034452-main/src/main/java/ch7/Cat.java
+++
@@ -1,6 +0,0 @@
-package ch7;
-
-// This class defines a Cat that inherits all behaviors from the Animal class without adding any new properties or methods.
-
-public class Cat extends Animal {
-}
