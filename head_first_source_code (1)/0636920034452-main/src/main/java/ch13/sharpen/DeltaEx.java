27845bb1d6ec0a5054c1bc921d53e37c6305249b
Kobeng Chantel <kobengchantel@gmail.com>
2025-12-31 21:42:00 +0200

    done with java

--- head_first_source_code (1)/0636920034452-main/src/main/java/ch13/sharpen/DeltaEx.java
+++
@@ -1,8 +0,0 @@
-package ch13.sharpen;
-
-// This class defines a custom checked exception by extending the base Exception class in Java.
-
-public class DeltaEx extends Exception {
-    // No constructors or methods defined yet,
-    // so it uses the default constructor from Exception.
-}
