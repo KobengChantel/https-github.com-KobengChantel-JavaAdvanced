a5489c114e8f50b572847684481ec4c3238f48fe
Kobeng Chantel <kobengchantel@gmail.com>
2026-01-19 23:13:07 +0200

    done with java

---
+++ Soccer/play/IDisplayDataItem.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package chapter15.soccer.play;
+
+/**
+ * @author ksomervi
+ */
+public interface IDisplayDataItem {
+
+    public boolean isDetailAvailable();
+
+    public String getDisplayDetail();
+
+    public int getID();
+
+    public String getDetailType();    // return simple text String or return the actual object?
+
+}
